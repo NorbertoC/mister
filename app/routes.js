21e39330f9cdb9f3af4601e9b3e2c92b809cca4d
Norberto Carosella <Norberto Carosella>
2016-07-29 18:55:24 -0300

    pruebas y mas pruebas

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,45 @@
 
   angular.module('myApp')
     .config(function($stateProvider, $urlRouterProvider) {
-      //
-      // For any unmatched url, redirect to /state1
-      $urlRouterProvider.otherwise("/home");
-      //
-      // Now set up the states
+
       $stateProvider
         .state('home', {
           url: "/home",
-          templateUrl: "shopping-list/shopping-list.template.html"
+          templateUrl: "shopping-list/home.html"
         })
+        .state('list', {
+          url: '/list',
+          templateUrl: 'shopping-list/list.html',
+          controller: 'ListCtrl'
+        })
+        .state('list.item', {
+          url: '/:item',
+          templateUrl: 'shopping-list/list.item.html',
+          controller: function($scope, $stateParams) {
+            $scope.item = $stateParams.item;
+          }
+        });
+    })
 
-    });
+
+
+  .controller('ListCtrl', function($scope) {
+    $scope.shoppingList = [
+      { name: 'Milk' },
+      { name: 'Eggs' },
+      { name: 'Bread' },
+      { name: 'Cheese' },
+      { name: 'Ham' }
+    ];
+
+    $scope.selectItem = function(selectedItem) {
+      _($scope.shoppingList).each(function(item) {
+        item.selected = false;
+        if (selectedItem === item) {
+          selectedItem.selected = true;
+        }
+      });
+    };
+  })
 
 })();
