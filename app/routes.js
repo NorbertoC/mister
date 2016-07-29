fab2560f7db2fa744b09937824ec34e25a91f6a0
Norberto Carosella <Norberto Carosella>
2016-07-29 20:15:59 -0300

    asdf

--- app/routes.js
+++ app/routes.js
@@ -1,75 +1,18 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('myApp')
-<<<<<<< HEAD
-      .config(function($stateProvider, $urlRouterProvider) {
-=======
-    .config(function($stateProvider, $urlRouterProvider) {
-      //
-      // For any unmatched url, redirect to /state1
-      $urlRouterProvider.otherwise("/home");
-      //
-      // Now set up the states
-      $stateProvider
-        .state('home', {
-          url: "/home",
-          templateUrl: "shopping-list/home.html"
-        })
-        .state('list', {
-          url: '/list',
-          templateUrl: 'shopping-list/list.html',
-          controller: 'ListCtrl'
-        }).
-        state('list.item', {
-          url: '/:item',
-          templateUrl: 'shopping-list/list.item.html',
-          controller: function($scope, $stateParams) {
-            $scope.item = $stateParams.item;
-          }
-        })
-
-      ;
->>>>>>> c38dbd17d5cb9dde25c8dc072dad22e4c485c1f6
-
-        $stateProvider
-            .state('home', {
-              url: "/home",
-              templateUrl: "shopping-list/home.html"
-            })
-            .state('list', {
-              url: '/list',
-              templateUrl: 'shopping-list/list.html',
-              controller: 'ListCtrl'
-            })
-            .state('list.item', {
-              url: '/:item',
-              templateUrl: 'shopping-list/list.item.html',
-              controller: function($scope, $stateParams) {
-                $scope.item = $stateParams.item;
-              }
-            });
-      })
-
-
-
-      .controller('ListCtrl', function($scope) {
-        $scope.shoppingList = [
-          { name: 'Milk' },
-          { name: 'Eggs' },
-          { name: 'Bread' },
-          { name: 'Cheese' },
-          { name: 'Ham' }
-        ];
-
-        $scope.selectItem = function(selectedItem) {
-          _($scope.shoppingList).each(function(item) {
-            item.selected = false;
-            if (selectedItem === item) {
-              selectedItem.selected = true;
-            }
-          });
-        };
-      })
+    angular.module('myApp')
+        .config(function($stateProvider, $urlRouterProvider) {
+            //
+            // For any unmatched url, redirect to /state1
+            $urlRouterProvider.otherwise("/home");
+            //
+            // Now set up the states
+            $stateProvider
+                .state('home', {
+                    url: "/home",
+                    templateUrl: "shopping-list/home.html"
+                })
+        });
 
 })();
