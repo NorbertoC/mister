fcc7d0631e88b26c1380efdb2622e4928edcec35
Norberto Carosella <Norberto Carosella>
2016-07-30 20:11:37 -0300

    routes

--- app/common/routes.js
+++ app/common/routes.js
@@ -21,6 +21,31 @@
                     url: '/pizzas',
                     templateUrl: 'app/categories/comidas/pizzas/pizzas.html'
                 })
+                .state('lomitos', {
+                    url: '/lomitos',
+                    templateUrl: 'app/categories/comidas/lomitos/lomitos.html'
+                })
+                .state('menu-del-dia', {
+                    url: '/menu-del-dia',
+                    templateUrl: 'app/categories/comidas/menu-del-dia/menu-del-dia.html'
+                })
+                .state('tartas', {
+                    url: '/tartas',
+                    templateUrl: 'app/categories/comidas/tartas/tartas.html'
+                })
+                .state('contacto', {
+                    url: '/contacto',
+                    templateUrl: 'app/categories/contacto/contactanos.html'
+                })
+                .state('nosotros', {
+                    url: '/nosotros',
+                    templateUrl: 'app/categories/nosotros/nosotros.html'
+                })
+                .state('novedades', {
+                    url: '/novedades',
+                    templateUrl: 'app/categories/novedades/novedades.html'
+                })
+
 
             ;
 
